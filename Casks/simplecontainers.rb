ac58d87e753cc6f1b8029e160df06e99f7e5f388
Amaury Rousseau <contact@thekinrar.fr>
2026-05-02 03:25:27 +0200

    Add verified url parameter

--- Casks/simplecontainers.rb
+++ Casks/simplecontainers.rb
@@ -5,7 +5,8 @@ cask "simplecontainers" do
   sha256 arm: "0072b8f1971badf1177fcc01ff69564658cf6a38daba7063883678f94a2e28aa",
          intel: "b07a561623b8ddc919a94981aef6327b7391958b85e1cdaa2185bc4dbd9f03a6"
 
-  url "https://files.simplecontainers.app/cli/rel/#{version}/simplecontainers-cli-#{version}-darwin-#{arch}"
+  url "https://files.simplecontainers.app/cli/rel/#{version}/simplecontainers-cli-#{version}-darwin-#{arch}",
+      verified: "files.simplecontainers.app"
   name "Simple Containers"
   desc "Simple Containers CLI client"
   homepage "simplecontainers.app"
